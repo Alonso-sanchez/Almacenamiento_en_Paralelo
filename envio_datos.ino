50da9a5ac6b329feb56acb53770ea08098279e95
Alonso Sanchez <stmalonsoso@gmail.com>
2020-12-10 17:32:38 -0600

    Codigo de arduino que envia los datos recolectados a la BD

--- envio_datos.ino
+++ envio_datos.ino
@@ -2,8 +2,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-
-// Variables
+#include <Servo.h>
+Servo servoMotor;
+int i = 0;
 int StadoIzq = 5;
 int StadoDch = 4;
 int ledIzq = 14;
@@ -23,6 +24,7 @@ void setup()
   pinMode(StadoDch, INPUT);
   pinMode(ledIzq, OUTPUT);
   pinMode(ledDer, OUTPUT);
+  servoMotor.attach(14);
 
   Serial.begin(9600);
 
@@ -44,64 +46,72 @@ void loop() {
   { //Check WiFi connection status
 
     HTTPClient http;
-    http.begin("http://192.168.0.19:3000/device/");
+    http.begin("http://144.202.34.148:6969/device/");
 
     int httpCode = http.GET();
     String payload = http.getString();
-    if (httpCode > 0)
-    {
+    if (httpCode > 0) {
       String payload2 = payload;
       char json[500];
       payload2.toCharArray(json, 500);
-
-      Serial.println("Respuesta de la peticion");
-      Serial.println(payload2);
+      //      Serial.println(payload2);
       StaticJsonDocument<256> doc;
 
       deserializeJson(doc, json);
 
       String estado = doc["estado"];
+      int angulo = doc["angulo"];
 
 
-      Serial.println("Imprimiendo el estado del apagador");
-      estadoIzquierda = digitalRead(StadoIzq);
-      estadoDerecha = digitalRead(StadoDch);
+      // Serial.println("Imprimiendo el estado del apagador");
+      for (i; i < 180; i++)
+      {
+        servoMotor.write(i);
+        estadoIzquierda = digitalRead(StadoIzq);
+        estadoDerecha = digitalRead(StadoDch);
 
-      if ((estadoActual != 1) and (estadoIzquierda == HIGH)) {
-        estadoActual = 1;
-        Serial.println("Izquierda");
-        digitalWrite(ledIzq, HIGH);
-        digitalWrite(ledDer, LOW);
-        post("Izquierda");
+        if ((estadoActual != 1) and (estadoIzquierda == HIGH)) {
+          estadoActual = 1;
+          Serial.println("Izquierda");
+          digitalWrite(ledIzq, HIGH);
+          digitalWrite(ledDer, LOW);
+          post("Izquierda", i);
+        }
+        if ((estadoActual != 3) and (estadoDerecha == HIGH)) {
+          estadoActual = 3;
+          Serial.println("Derecha");
+          digitalWrite(ledIzq, LOW);
+          digitalWrite(ledDer, HIGH);
+          post("Derecha", i);
+        }
+        if ((estadoActual != 2) and (estadoDerecha == LOW ) and (estadoIzquierda == LOW)) {
+          estadoActual = 2;
+          Serial.println("Centro");
+          digitalWrite(ledIzq, LOW);
+          digitalWrite(ledDer, LOW);
+          post("Centro", i);
+        }
+        delay(25);
       }
-      if ((estadoActual != 3) and (estadoDerecha == HIGH)) {
-        estadoActual = 3;
-        Serial.println("Derecha");
-        digitalWrite(ledIzq, LOW);
-        digitalWrite(ledDer, HIGH);
-        post("Derecha");
-      }
-      if ((estadoActual != 2) and (estadoDerecha == LOW ) and (estadoIzquierda == LOW)) {
-        estadoActual = 2;
-        Serial.println("Centro");
-        digitalWrite(ledIzq, LOW);
-        digitalWrite(ledDer, LOW);
-        post("Centro");
-      }
+      for (i; i > 0; i--)
+      {
+        servoMotor.write(i);
 
-      delay(200);
+      }
+      //delay(800);
     }
   }
 }
 
-void post(String estado) {
+void post(String estado, int angulo) {
   Serial.println("Inicio post");
   HTTPClient http;
   String json;
-  server = "http://192.168.0.19:3000/device/";
+  server = "http://144.202.34.148:6969/device/";
 
   StaticJsonDocument<256> doc;
   doc["estado"] = String(estado);
+  doc["angulo"] = int(angulo);
   serializeJson(doc, json);
 
   http.begin(server);
